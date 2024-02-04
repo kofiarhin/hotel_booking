ae5d76bf2f2f0a2a6153cd69c4fefaf0b38fd849
kofi <kofiarhin69@gmail.com>
2024-02-04 16:11:48 +0000

    added client with redux template

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const getRooms = async () => {
+      const res = await fetch("/rooms");
+      if (res.ok) {
+        const data = await res.json();
+
+        console.log(data);
+      }
+    };
+
+    getRooms();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
-}
+};
 
 export default App;

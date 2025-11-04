a0daaafa37c4c7cf3c2ece8f304c27c3cf674c24
Kofi Arhin <kofiarhin69@gmail.com>
2025-11-04 04:55:48 +0000

    refactor: migrate client to vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,17 @@
-import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.scss";
-import Home from "./Pages/Home/Home";
-import Header from "./components/Header/Header";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-import Room from "./Pages/Room/Room";
-import Rooms from "./Pages/Rooms";
-import Footer from "./components/Footer/Footer";
-import Bookings from "./Pages/Bookings/Bookings";
-import Success from "./Pages/Success/Success";
-import CreateBooking from "./Pages/CreateBooking/CreateBooking";
-import Spa from "./Pages/Spa/Spa";
-import Dining from "./Pages/Dining/Dining";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.scss';
+import Home from './Pages/Home/Home';
+import Header from './components/Header/Header';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Room from './Pages/Room/Room';
+import Rooms from './Pages/Rooms';
+import Footer from './components/Footer/Footer';
+import Bookings from './Pages/Bookings/Bookings';
+import Success from './Pages/Success/Success';
+import CreateBooking from './Pages/CreateBooking/CreateBooking';
+import Spa from './Pages/Spa/Spa';
+import Dining from './Pages/Dining/Dining';
 
 const App = () => {
   return (
